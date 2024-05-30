8e6b15ad1a1e5ca5001f2a60d951d1aee572cd28
Smbat <smbattumasyan@gmail.com>
2024-05-30 20:06:06 +0400

    removed comments.

--- PhotoSearch/Common/FavoritesManager.swift
+++ PhotoSearch/Common/FavoritesManager.swift
@@ -5,28 +5,6 @@
 //  Created by Smbat Tumasyan on 30.05.24.
 //
 
-//import Foundation
-//
-//class FavoritesManager {
-//    static let shared = FavoritesManager()
-//    private var likedPhotos: [Photo] = []
-//    
-//    private init() {}
-//    
-//    func addPhoto(_ photo: Photo) {
-//        likedPhotos.append(photo)
-//    }
-//    
-//    func removePhoto(_ photo: Photo) {
-//        likedPhotos.removeAll { $0.id == photo.id }
-//    }
-//    
-//    func getLikedPhotos() -> [Photo] {
-//        return likedPhotos
-//    }
-//}
-
-
 import Foundation
 import Combine
 
